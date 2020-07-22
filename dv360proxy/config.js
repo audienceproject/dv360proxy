d7f66fbe21f2d9854230b54ea37b4bf8c9133df8
Roman Gomolko <rg@audienceproject.com>
2020-07-22 07:31:55 +0300

    Moved SSM parameter names to Lambda env variables populated by template parameter

--- dv360proxy/config.js
+++ dv360proxy/config.js
@@ -2,19 +2,22 @@ var AWS = require("aws-sdk");
 var ssm = new AWS.SSM();
 
 module.exports = async function () {
+    const credentialsParameterName = process.env.API_CREDENTIALS_PARAMETER_NAME;
+    const configParameterName = process.env.CONFIG_PARAMETER_NAME;
+
     const ssmResponse = await ssm.getParameters({
         "Names": [
-            "dv360proxy.credentials",
-            "dv360proxy.config"
+            credentialsParameterName,
+            configParameterName
         ],
         "WithDecryption": true
     }).promise();
     if (ssmResponse.InvalidParameters.length > 0) {
-        throw new Error("Unable to read SSM parameters", ssmResponse.InvalidParameters.join(", "));
+        throw new Error(`Unable to read SSM parameters ${ssmResponse.InvalidParameters.join(", ")}`);
     }
 
-    const credentialsJSON = ssmResponse.Parameters.find(p => p.Name === "dv360proxy.credentials").Value;
-    const configJSON = ssmResponse.Parameters.find(p => p.Name === "dv360proxy.config").Value;
+    const credentialsJSON = ssmResponse.Parameters.find(p => p.Name === credentialsParameterName).Value;
+    const configJSON = ssmResponse.Parameters.find(p => p.Name === configParameterName).Value;
 
     return {
         credentials: JSON.parse(credentialsJSON),
